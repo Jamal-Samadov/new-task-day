0d571c083320b40b11c49e757444b8c29b5977d1
Jamaltscode <jamalts@code.edu.az>
2022-04-18 17:23:37 +0400

    new task dowloaded

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divided not 19 / 7");
+
+            int result = (21);
+            
+           
+            if (result % 7 == 0)
+                {
+                Console.WriteLine(result);
+                }
+        }
+    }
+}
---
+++ TassKK3/TassKK3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TassKK3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number;
+           
+            Console.Write("Divided number 46  \n ");
+            
+            number = Convert.ToInt32(Console.ReadLine());
+           
+            Console.Write("\nAll Number  \n");
+            
+            for (int i = 3; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
